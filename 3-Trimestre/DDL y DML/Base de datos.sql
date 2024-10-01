880a3bdf133e2840737b1bb71c1b51404ca46997
gerson <163358849+Gersoncorredor@users.noreply.github.com>
2024-09-30 20:47:32 -0500

    Update Base de datos.sql

--- 3-Trimestre/DDL y DML/Base de datos.sql
+++ 3-Trimestre/DDL y DML/Base de datos.sql
@@ -319,169 +319,183 @@ GRANT 'desarollador' TO 'joan'@'localhost';
 grant "usuario" to "kevin"@"localhost";
 FLUSH PRIVILEGES;
 
-/* 1) Listar todas las citas de un cliente específico, junto con el nombre del oftalmólogo asignado:*/
+/*Consultas con INNER JOIN*/
 
-SELECT c.idCita, 
-       pCliente.primerNombre AS nombre_cliente, 
-       pOftalmologo.primerNombre AS nombre_oftalmologo, 
-       c.descripcion
-FROM cita c
-JOIN cliente cl ON c.clienteNumeroDocumento = cl.numeroDocumento
-JOIN persona pCliente ON cl.numeroDocumento = pCliente.numeroDocumento
-JOIN oftalmologo o ON c.oftalmologoNumeroDocumento = o.numeroDocumento
-JOIN persona pOftalmologo ON o.numeroDocumento = pOftalmologo.numeroDocumento
-WHERE pCliente.numeroDocumento = 12345678;
+/*Buscar el cliente con su primera cita programada pendiente*/
+select p.primerNombre, p.primerApellido, c.idCita, c.idCita, es.nombre AS Estado_Cita
+FROM persona p
+INNER JOIN cliente ON p.numeroDocumento = cliente.numeroDocumento AND p.idTipoIdentificacion = cliente.idTipoIdentificacion
+INNER JOIN cita c ON c.clienteNumeroDocumento = cliente.numeroDocumento
+INNER JOIN detallecita ON detallecita.idCita = c.idCita
+INNER JOIN estadoscita es ON detallecita.idEstadoCita = es.idEstadocita 
+where es.idEstadocita = "1" AND c.clienteNumeroDocumento = "12345678" limit 1;
 
-/* 2) Obtener todas las citas programadas para un oftalmólogo específico:*/
-SELECT c.idCita, p.primerNombre AS nombre_cliente, c.descripcion, h.fecha, h.hora
-FROM cita c
-JOIN cliente cl ON c.clienteNumeroDocumento = cl.numeroDocumento
-JOIN persona p ON cl.numeroDocumento = p.numeroDocumento
-JOIN Horario h ON c.idCita = h.idHorarios
-WHERE c.oftalmologoNumeroDocumento = 61452345;
+/*Obtener todos los diagnósticos de un cliente específico*/
+select p.primerNombre, p.primerApellido, d.idDiagnostico AS Diagnostico, d.descripcion
+FROM persona p
+INNER JOIN diagnostico d ON p.numeroDocumento = d.numeroDocumento AND p.idTipoIdentificacion = d.tipoDocumento where d.numeroDocumento like "98742135";
 
-/* 3) Mostrar todas las citas realizadas junto con el estado de la cita y el diagnóstico:*/
-SELECT c.idCita, p.primerNombre, d.descripcion AS diagnostico, e.nombre AS estado_cita
-FROM cita c
-JOIN detalleCita dc ON c.idCita = dc.idCita
-JOIN diagnostico d ON dc.idDiagnostico = d.idDiagnostico
-JOIN estadosCita e ON dc.idEstadoCita = e.idEstadocita
-JOIN persona p ON c.clienteNumeroDocumento = p.numeroDocumento
-WHERE e.nombre = 'Realizada';
-
-/* 4) Listar todos los clientes que han solicitado un tipo específico de consulta (e.g., "Gafas y Monturas"):*/
-
-SELECT p.primerNombre, p.primerApellido, tc.nombre AS tipo_consulta
-FROM detalleCita dc
-JOIN tipoConsulta tc ON dc.idTipoConsulta = tc.idtipoConsulta
-JOIN cita c ON dc.idCita = c.idCita
-JOIN persona p ON c.clienteNumeroDocumento = p.numeroDocumento
-WHERE tc.nombre = 'Gafas y Monturas';
-
-/* 5) Listar todas las citas pendientes con sus horarios y dirección:*/
-
-SELECT p.primerNombre AS cliente, h.fecha, h.hora, dc.direccion
-FROM detalleCita dc
-JOIN cita c ON dc.idCita = c.idCita
-JOIN persona p ON c.clienteNumeroDocumento = p.numeroDocumento
-JOIN Horario h ON dc.idHorarios = h.idHorarios
-JOIN estadosCita e ON dc.idEstadoCita = e.idEstadocita
-WHERE e.nombre = 'Pendiente';
-
-/* 6) Obtener el historial clínico de un cliente junto con su información personal:*/
-
-SELECT p.primerNombre, p.primerApellido, h.numeroDocumento, d.descripcion AS diagnostico
-FROM historialClinico h
-JOIN persona p ON h.numeroDocumento = p.numeroDocumento
-JOIN diagnostico d ON h.numeroDocumento = d.numeroDocumento
-WHERE p.numeroDocumento = 12345678;
-
-/* 7) Listar todos los empleados y sus respectivos roles:*/
-SELECT p.primerNombre, p.primerApellido, re.nombre AS rol
+/*Listar todos los empleados y su rol correspondiente*/
+SELECT p.primerNombre, p.primerApellido, r.nombre AS rol
 FROM empleado e
-JOIN Empleado_has_rolEmpleado er ON e.numeroDocumento = er.numeroDocumento
-JOIN rolEmpleado re ON er.idRol = re.idRol
-JOIN persona p ON e.numeroDocumento = p.numeroDocumento;
+INNER JOIN persona p ON e.numeroDocumento = p.numeroDocumento AND e.idTipoIdentificacion = p.idTipoIdentificacion
+INNER JOIN Empleado_has_rolEmpleado er ON e.numeroDocumento = er.numeroDocumento AND e.idTipoIdentificacion = er.idTipoIdentificacion
+INNER JOIN rolEmpleado r ON er.idRol = r.idRol
+ORDER BY p.primerNombre;
 
-/* 8) Obtener todas las citas asignadas a un oftalmólogo en una fecha específica:*/
-
-SELECT p.primerNombre AS cliente, h.fecha, h.hora
-FROM cita c
-JOIN detalleCita dc ON c.idCita = dc.idCita
-JOIN Horario h ON dc.idHorarios = h.idHorarios
-JOIN persona p ON c.clienteNumeroDocumento = p.numeroDocumento
-WHERE c.oftalmologoNumeroDocumento = 61452345 AND h.fecha = '2024-09-21';
-
-/* 9) Obtener los clientes con más de 3 citas agendadas:*/
-
-SELECT p.primerNombre, p.primerApellido, COUNT(c.idCita) AS numero_citas
-FROM cita c
-JOIN persona p ON c.clienteNumeroDocumento = p.numeroDocumento
-GROUP BY p.numeroDocumento
-HAVING COUNT(c.idCita) > 3;
-
-/* 10) Mostrar todos los problemas reportados por un cliente y el tipo de problema:*/
-
-SELECT p.primerNombre, p.primerApellido, tp.descripcion AS tipo_problema, s.descripcionProblema
+/*Buscar todos los problemas de soporte para un cliente específico*/
+SELECT s.descripcionProblema, tp.descripcion AS Tipo_Problema
 FROM soporte s
-JOIN tipoProblema tp ON s.idTipoProblema = tp.idTipoProblema
-JOIN cliente c ON s.numeroDocumento = c.numeroDocumento
-JOIN persona p ON c.numeroDocumento = p.numeroDocumento
-WHERE p.numeroDocumento = 12345678;
+INNER JOIN tipoProblema tp ON s.idTipoProblema = tp.idTipoProblema
+INNER JOIN cliente c ON s.numeroDocumento = c.numeroDocumento
+WHERE c.numeroDocumento = 98512478
+ORDER BY s.idSoporte;
 
--- Consultas aninadas
-/* 1) Obtener el número total de citas realizadas por cada oftalmólogo:*/
+/*Obtener la cantidad de citas pendientes por oftalmólogo:*/
+SELECT o.numeroDocumento, COUNT(ci.idCita) AS cantidadPendientes, ec.nombre AS Estado_citas
+FROM oftalmologo o
+INNER JOIN cita ci ON o.numeroDocumento = ci.oftalmologoNumeroDocumento
+INNER JOIN detallecita dc ON ci.idCita = dc.idCita
+INNER JOIN estadosCita ec ON dc.idEstadoCita = ec.idEstadocita
+WHERE ec.nombre = 'Pendiente'
+GROUP BY o.numeroDocumento
+ORDER BY cantidadPendientes DESC;
 
-SELECT p.primerNombre, p.primerApellido, (SELECT COUNT(c.idCita) FROM cita c WHERE c.oftalmologoNumeroDocumento = o.numeroDocumento) AS total_citas
-FROM persona p
-JOIN oftalmologo o ON p.numeroDocumento = o.numeroDocumento;
+/*LEft  */
 
-/* 2) Obtener el nombre del cliente que más citas ha realizado: */
+/* Contar el número de citas por cliente, incluso si algunos clientes no tienen citas asignadas */
+SELECT
+    p.primerNombre AS ClienteNombre,
+    p.primerApellido AS ClienteApellido,
+    COUNT(c.idCita) AS TotalCitas
+FROM
+    cliente cl
+    LEFT JOIN persona p ON cl.numeroDocumento = p.numeroDocumento
+    LEFT JOIN cita c ON cl.numeroDocumento = c.clienteNumeroDocumento
+GROUP BY
+    p.primerNombre,
+    p.primerApellido;
 
-SELECT p.primerNombre, p.primerApellido
-FROM persona p
-WHERE p.numeroDocumento = (SELECT c.clienteNumeroDocumento FROM cita c GROUP BY c.clienteNumeroDocumento ORDER BY COUNT(c.idCita) DESC LIMIT 1);
+/*Obtener la fecha más reciente de una cita por cada cliente */
+SELECT
+    p.primerNombre AS ClienteNombre,
+    p.primerApellido AS ClienteApellido,
+    MAX(h.fecha) AS UltimaFechaCita
+FROM
+    cliente cl
+    LEFT JOIN persona p ON cl.numeroDocumento = p.numeroDocumento
+    LEFT JOIN cita c ON cl.numeroDocumento = c.clienteNumeroDocumento
+    LEFT JOIN detalleCita dc ON c.idCita = dc.idCita
+    LEFT JOIN Horario h ON dc.idHorarios = h.idHorarios
+GROUP BY
+    p.primerNombre,
+    p.primerApellido;
 
-/* 3) Listar todas las citas que están pendientes y fueron agendadas por clientes con más de 2 citas:*/
-SELECT c.idCita, p.primerNombre, e.nombre AS estado_cita
-FROM cita c
-JOIN persona p ON c.clienteNumeroDocumento = p.numeroDocumento
-JOIN detalleCita dc ON c.idCita = dc.idCita
-JOIN estadosCita e ON dc.idEstadoCita = e.idEstadocita
-WHERE e.nombre = 'Pendiente' AND c.clienteNumeroDocumento IN 
-(SELECT clienteNumeroDocumento FROM cita GROUP BY clienteNumeroDocumento HAVING COUNT(idCita) > 2);
 
-/* 4) Mostrar la dirección de la última cita agendada para un cliente específico:*/
-SELECT dc.direccion
-FROM detalleCita dc
-WHERE dc.idCita = (SELECT MAX(c.idCita) FROM cita c WHERE c.clienteNumeroDocumento = 12345678);
+/*RIGHT*/
 
-/* 5) Obtener el promedio de citas realizadas por cliente:*/
+/*Consultar las citas donde su descripción tengan consulta*/
+SELECT ct.idCita AS 'ID de Cita', pCliente.primerNombre AS 'Nombre Cliente', pOftalmologo.primerNombre AS 'Nombre Oftalmólogo', ct.descripcion AS 'Descripción de la Cita'
+FROM
+    cita ct
+    RIGHT JOIN persona pCliente ON ct.clienteNumeroDocumento = pCliente.numeroDocumento
+    RIGHT JOIN oftalmologo o ON ct.oftalmologoNumeroDocumento = o.numeroDocumento
+    JOIN persona pOftalmologo ON o.numeroDocumento = pOftalmologo.numeroDocumento
+WHERE
+    ct.descripcion LIKE '%consulta%';
 
-SELECT AVG(citas_por_cliente) AS promedio_citas
-FROM (SELECT COUNT(c.idCita) AS citas_por_cliente FROM cita c GROUP BY c.clienteNumeroDocumento) AS subquery;
+/* Se requiere obtener un reporte que incluya todas las citas programadas */
+SELECT
+    c.idCita AS NumeroCita,
+    c.descripcion AS Descripcion,
+    d.descripcion AS Diagnostico,
+    ec.nombre AS Estado
+FROM
+    estadosCita ec
+    RIGHT JOIN detalleCita dc ON ec.idEstadocita = dc.idEstadoCita
+    RIGHT JOIN cita c ON dc.idCita = c.idCita
+    LEFT JOIN diagnostico d ON dc.idDiagnostico = d.idDiagnostico;
 
-/* 6) Listar todos los empleados que no tienen asignado ningún rol:*/
+/* Se requiere visualisar todas las citas que estan en estado "Pendiente" */
+SELECT
+    c.idCita AS NumeroCita,
+    p.primerNombre AS NombrePaciente,
+    p.primerApellido AS ApellidoPaciente,
+    ec.nombre AS Estado
+FROM
+    estadosCita ec
+    RIGHT JOIN detalleCita dc ON ec.idEstadocita = dc.idEstadoCita
+    RIGHT JOIN cita c ON dc.idCita = c.idCita
+    RIGHT JOIN cliente cl ON c.clienteNumeroDocumento = cl.numeroDocumento
+    RIGHT JOIN persona p ON cl.numeroDocumento = p.numeroDocumento
+WHERE
+    ec.nombre LIKE 'Pendiente'
+    OR ec.nombre IS NULL;
 
-SELECT p.primerNombre, p.primerApellido
-FROM persona p
-WHERE p.numeroDocumento IN 
-(SELECT e.numeroDocumento FROM empleado e WHERE NOT EXISTS (SELECT * FROM Empleado_has_rolEmpleado er WHERE er.numeroDocumento = e.numeroDocumento));
+/*Consultas anidadas*/
 
-/* 7) Mostrar el nombre del cliente con la cita más reciente:*/
+/*Obtener el oftalmólogo que ha atendido la mayor cantidad de citas*/
+SELECT o.numeroDocumento, p.primerNombre, p.primerApellido
+FROM oftalmologo o
+INNER JOIN persona p ON o.numeroDocumento = p.numeroDocumento
+WHERE o.numeroDocumento = (
+        SELECT c.oftalmologoNumeroDocumento
+        FROM cita c
+        GROUP BY c.oftalmologoNumeroDocumento
+        ORDER BY COUNT(c.idCita) DESC
+        LIMIT 1
+		);
 
-SELECT p.primerNombre, p.primerApellido
-FROM persona p
-JOIN cliente cl ON p.numeroDocumento = cl.numeroDocumento
-WHERE p.numeroDocumento = (SELECT c.clienteNumeroDocumento FROM cita c ORDER BY c.idCita DESC LIMIT 1);
 
-/* 8)  Obtener los empleados que tienen más de un rol asignado:*/
+/*Obtener el oftalmólogo que ha atendido la mayor cantidad de citas, junto con sus detalles, 
+filtrado por el nombre del oftalmólogo que contenga "Carlos" en su nombre.*/
+SELECT p.primerNombre, p.primerApellido, o.numeroDocumento, COUNT(c.idCita) AS totalCitas
+FROM oftalmologo o
+INNER JOIN persona p ON o.numeroDocumento = p.numeroDocumento
+INNER JOIN cita c ON o.numeroDocumento = c.oftalmologoNumeroDocumento
+WHERE p.primerNombre LIKE '%Carlos%' AND p.idRol = 2
+GROUP BY o.numeroDocumento
+ORDER BY totalCitas DESC;
 
-SELECT p.primerNombre, p.primerApellido
-FROM persona p
-JOIN empleado e ON p.numeroDocumento = e.numeroDocumento
-WHERE (SELECT COUNT(*) FROM Empleado_has_rolEmpleado er WHERE er.numeroDocumento = e.numeroDocumento) > 1;
-/* 9) Listar las citas con un diagnóstico específico y su estado:*/
-SELECT p.primerNombre, p.primerApellido, e.nombre AS estado_cita
-FROM persona p
-JOIN cita c ON p.numeroDocumento = c.clienteNumeroDocumento
-JOIN detalleCita dc ON c.idCita = dc.idCita
-JOIN estadosCita e ON dc.idEstadoCita = e.idEstadocita
-WHERE dc.idDiagnostico = (SELECT d.idDiagnostico FROM diagnostico d WHERE d.descripcion LIKE '%miopía%');
+/*Obtener el cliente que ha registrado más problemas de soporte relacionados con "historial clínico"*/
+SELECT p.primerNombre, p.primerApellido, s.numeroDocumento, COUNT(s.idSoporte) AS totalSoportes, s.descripcionProblema
+FROM soporte s
+INNER JOIN persona p ON s.numeroDocumento = p.numeroDocumento
+WHERE s.idTipoProblema = (
+        SELECT idTipoProblema 
+        FROM tipoProblema 
+        WHERE descripcion LIKE '%historial clínico%'
+		)
+GROUP BY s.numeroDocumento
+ORDER BY totalSoportes DESC;
 
-/* 10) Obtener el número total de citas canceladas:*/
-
-SELECT COUNT(c.idCita) AS total_canceladas
-FROM cita c
-JOIN detalleCita dc ON c.idCita = dc.idCita
-WHERE dc.idEstadoCita = (SELECT e.idEstadocita FROM estadosCita e WHERE e.nombre = 'Cancelada');
-
-/* 11) Mostrar las citas que han sido asignadas a oftalmólogos que han gestionado más de 2 citas.*/
-SELECT * 
-FROM cita 
-WHERE oftalmologoNumeroDocumento IN (
-    SELECT oftalmologoNumeroDocumento 
+/*Obtener las descripciones de diagnósticos realizados para clientes que hayan tenido 
+una cita programada con fecha posterior a '2024-09-22'*/
+SELECT descripcion
+FROM diagnostico
+WHERE numeroDocumento IN (
+    SELECT clienteNumeroDocumento 
     FROM cita 
-    GROUP BY oftalmologoNumeroDocumento 
-    HAVING COUNT(*) > 2
-);
+    WHERE idCita IN (
+        SELECT idCita 
+        FROM detalleCita 
+        WHERE idHorarios IN (
+            SELECT idHorarios 
+            FROM Horario 
+            WHERE fecha > '2024-09-22'
+        )
+    )
+)
+ORDER BY idDiagnostico;
+
+/*Obtener los clientes que tienen más de 1 cita programada, mostrando su número de documento 
+y cantidad de citas, y ordenado por el número de citas*/
+SELECT clienteNumeroDocumento, COUNT(idCita) AS totalCitas
+FROM cita
+WHERE clienteNumeroDocumento IN (
+    SELECT numeroDocumento 
+    FROM cliente
+)
+GROUP BY clienteNumeroDocumento
+HAVING COUNT(idCita) > 1
+ORDER BY totalCitas DESC;
