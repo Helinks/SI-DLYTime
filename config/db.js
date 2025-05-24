335b7914f7bafbadd95868b1cdf7a80905fc2b12
gerson <Jp1838883@gmail.com>
2025-05-24 15:39:20 -0500

    update config

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
-const mysql = require("mysql");
+const mysql = require('mysql')
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "dlytime",
-});
+  host: 'mysqldatabaseserverdlytime.mysql.database.azure.com',
+  user: 'administrador',
+  password: '123123Ge.',
+  database: 'dlyTime',
+  port: 3306,
+  ssl: { ca: fs.readFileSync('{ca-cert filename}') },
+})
 
-
-module.exports = db;
+module.exports = db
